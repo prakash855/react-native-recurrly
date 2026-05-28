508be7535fa0136ded13e1ad4e7af20ef5c9ac9f
Prakash Agrahari <sshivamprakashh301@gmail.com>
2026-05-28 13:17:46 +0530

    feat: nativewind, routing

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { styled } from 'nativewind';
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView)
+
+export default function App() {
+    return (
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text className="text-xl font-bold text-success">
+                Welcome to Nativewind!
+            </Text>
+            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4" >Go to onboarding</Link>
+            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4" >Sign in</Link>
+            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4" >Sign up</Link>
+            <Link href="/subscriptions/spotify" >Spotiy Subscription</Link>
+            <Link href={{
+                pathname: "/subscriptions/[id]",
+                params: { id: "claude" },
+            }}
+            >
+                Claude Max Subscription
+            </Link>
+        </SafeAreaView>
+    );
+}
---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const OnBoarding = () => {
+    return (
+        <View>
+            <Text>Onboarding</Text>
+        </View>
+    )
+}
+
+export default OnBoarding
